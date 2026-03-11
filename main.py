64419b5b35b494df7a767282ff89f0b92c1c13b0
Emilio <emilio.orellana@fce.uncu.edu.ar>
2026-03-11 17:24:02 -0300

    feat: extract audio

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src.video.extractor import download_video
+from src.video.extractor import extract_audio
 
 def main():
+    print("Downloading video...")
     url = "https://www.youtube.com/watch?v=RgpAc7U29BA"
     path = download_video(url)
     print(path)
+
+    print("Extracting audio...")
+    audio_path = extract_audio(path)
+    print(audio_path)
 
 if __name__ == "__main__":
     main()
--- src/video/extractor.py
+++ src/video/extractor.py
@@ -1,6 +1,8 @@
 from yt_dlp import YoutubeDL
 from src.config.settings import VIDEO_DIR
+from src.config.settings import AUDIO_DIR
 import os
+import subprocess
 
 def download_video(url: str) -> str:
     """
@@ -14,8 +16,11 @@ def download_video(url: str) -> str:
     """
     # Options
     os.makedirs(VIDEO_DIR, exist_ok=True)
+
     ydl_opts = {
         "outtmpl": f"{VIDEO_DIR}/%(title)s.%(ext)s",
+        "restrictfilenames": True,
+        "noplaylist": True,
     }
     
     # Download the video
@@ -36,7 +41,21 @@ def extract_audio(video_path: str) -> str:
     Returns:
         str: Path to the extracted audio file.
     """
+    # Options
+    os.makedirs(AUDIO_DIR, exist_ok=True)
+
+    filename = os.path.basename(video_path)
+    name, _ = os.path.splitext(filename)
+    audio_path = os.path.join(AUDIO_DIR, name + ".wav")
 
     # Extract audio
+    subprocess.run([
+        "ffmpeg",
+        "-i", video_path,
+        "-vn",
+        "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1",
+        audio_path
+    ], check=True)    
 
     # Return the audio file path
+    return audio_path   
